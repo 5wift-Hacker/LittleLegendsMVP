c412645f182a08938c1c410c9e8cc2b638dbbc5a
John Newman <john.newman920@gmail.com>
2025-08-18 15:58:32 +1000

    Initial Commit

---
+++ LittleLegends/LittleLegendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLegendsApp.swift
+//  LittleLegends
+//
+//  Created by John Newman on 18/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct LittleLegendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
